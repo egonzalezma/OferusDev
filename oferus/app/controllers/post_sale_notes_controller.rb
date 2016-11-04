ad3ee4a59f33fcb89a5b9b17201e7c5ae722eac8
egonzalezma <elias@oferus.com>
2016-11-04 13:47:21 -0300

    Fix 2.0 --rev

--- oferus/app/controllers/post_sale_notes_controller.rb
+++ oferus/app/controllers/post_sale_notes_controller.rb
@@ -59,9 +59,9 @@ class PostSaleNotesController < ApplicationController
 					"ttvta": [{"OrdeNum": "PRUEBAOFERUSb"+count+id ,
 						"ped_npv": "ST", 
 						"ped_domain": "ITAKA",
-						"ped_cust": "04319322",
-						"ped_bill": "04319322",
-						"ped_ship": "04319322",
+						"ped_cust": "UCVICUÑA",
+						"ped_bill": "UCVICUÑA ",
+						"ped_ship": "UCVICUÑA ",
 						"ped_ord_date": date,
 						"ped_crea_date": date,
 						"ped_crea_hour": hour,
@@ -97,85 +97,20 @@ class PostSaleNotesController < ApplicationController
 						},
 						{
 							"OrdeNum": "PRUEBAOFERUSb"+count+id,
-							"det_line": 1,
+							"det_line": 0,
 							"det_domain": "ITAKA",
-							"det_part": "101009",
+							"det_part": "999313",
 							"det_um": "PZ",
 							"det_qty_ord": 1,
-							"det_list_pr": 17500000 ,
-							"det_discount": 5,
-							"det_price": 12250000 
+							"det_list_pr": 6988,
+							"det_discount": 0,
+							"det_price": 6988 
 						}]
 					}]
 				}
 			}
 
-			bodyc = {
-				"dsOrder": {
-					"ttvta": [{"OrdeNum": "PRUEBAOFERUSc"+count+id ,
-						"ped_npv": "ST", 
-						"ped_domain": "ITAKA",
-						"ped_cust": "86053400",
-						"ped_bill": "86053400",
-						"ped_ship": "86053400",
-						"ped_ord_date": date,
-						"ped_crea_date": date,
-						"ped_crea_hour": hour,
-						"ped_genera_user": "KCATALAN",
-						"ped_crea_user": "KCATALAN",
-						"ped_aprueba_user": "KCATALAN",
-						"ped_exporta_date": date,
-						"ped_exporta_hour": hour,
-						"ped_exporta_user": "EALBORNO",
-						"ped_origen": "Directo",
-						"ped_retira": "Cliente",
-						"ped_po": "44552",
-						"ped_site": "2000",
-						"ped_slspsn": "300",
-						"ped_transporte": "0",
-						"ped_cr_terms": "CONTADO",
-						"ped_cr_card": "XF",
-						"ped_status": 9,
-						"ped_genera_date": date,
-						"ped_genera_hour": hour,
-						"ped_aprueba_date": date,
-						"ped_aprueba_hour": hour,
-						"ttdet": [{
-							"OrdeNum": "PRUEBAOFERUSc"+count+id ,
-							"det_line": 1,
-							"det_domain": "ITAKA",
-							"det_part": "123761",
-							"det_um": "PZ",
-							"det_qty_ord": 1,
-							"det_list_pr": 16308,
-							"det_discount": 50,
-							"det_price": 799092
-						},
-						{
-							"OrdeNum": "PRUEBAOFERUSc"+count+id ,
-							"det_line": 1,
-							"det_domain": "ITAKA",
-							"det_part": "101009",
-							"det_um": "PZ",
-							"det_qty_ord": 1,
-							"det_list_pr": 17500000,
-							"det_discount": 5,
-							"det_price": 12250000 
-						},
-					    {
-							"OrdeNum": "PRUEBAOFERUSc"+count+id ,
-							"det_line": 1,
-							"det_domain": "ITAKA",
-							"det_part": "103041",
-							"det_um": "PZ",
-							"det_qty_ord": 1,
-							"det_list_pr": 80357,
-							"det_discount": 10,
-							"det_price": 56250
-						}]
-					}]
-				}
-			}
+			
 
 			bodyd = {
 				"dsOrder": {
@@ -209,7 +144,7 @@ class PostSaleNotesController < ApplicationController
 						"ped_aprueba_hour": hour,
 						"ttdet": [{
 							"OrdeNum": "PRUEBAOFERUSd"+count+id ,
-							"det_line": 1,
+							"det_line": 0,
 							"det_domain": "ITAKA",
 							"det_part": "103050",
 							"det_um": "PZ",
@@ -254,7 +189,7 @@ class PostSaleNotesController < ApplicationController
 						"ped_aprueba_hour": hour,
 						"ttdet": [{
 							"OrdeNum": "PRUEBAOFERUSe"+count+id ,
-							"det_line": 1,
+							"det_line": 0,
 							"det_domain": "ITAKA",
 							"det_part": "103064",
 							"det_um": "PZ",
@@ -269,7 +204,7 @@ class PostSaleNotesController < ApplicationController
 
 		begin
 			sale_note = nil
-            modulo = @sale_notes.length%5
+            modulo = @sale_notes.length%4
             if modulo == 0
             	@response = RestClient.post(Datum::API_BASE_URL_SALE_NOTE , bodya.to_json, {content_type: :json, accept: :json})
                 sale_note = Sale::Note.save_from_erp_sale_note (bodya.to_json) 
@@ -281,28 +216,24 @@ class PostSaleNotesController < ApplicationController
             end
 
             if modulo == 2
-            	@response = RestClient.post(Datum::API_BASE_URL_SALE_NOTE , bodyc.to_json, {content_type: :json, accept: :json})
-            	sale_note = Sale::Note.save_from_erp_sale_note (bodyc.to_json)
-            end
-
-            if modulo == 3
             	@response = RestClient.post(Datum::API_BASE_URL_SALE_NOTE , bodyd.to_json, {content_type: :json, accept: :json})
             	sale_note = Sale::Note.save_from_erp_sale_note (bodyd.to_json)
             end
 
-            if modulo == 4
+            if modulo == 3
             	@response = RestClient.post(Datum::API_BASE_URL_SALE_NOTE , bodye.to_json, {content_type: :json, accept: :json})
             	sale_note = Sale::Note.save_from_erp_sale_note (bodye.to_json)
             end
+
 			
    			@sale_note = sale_note
 
 
 
-		@arr =	 { :m => "Nota de venta ingresada exitosamente a ERP"+modulo.to_s, :sn => @sale_note} 
+		@arr =	 { :m => "Nota de venta ingresada exitosamente a ERP Caso: "+modulo.to_s, :sn => @sale_note} 
 		
 		rescue => e
-		 @arr =	 { :m => "Problemas con el servicio Web ERP", :sn => @sale_note} 
+		 @arr =	 { :m => "Problemas con el servicio Web ERP Caso: "+modulo.to_s, :sn => @sale_note} 
 		end
 	end
 
